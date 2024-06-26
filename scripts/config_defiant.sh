ae1e9bbdc9b6e8227cb9df62971471adf8b891f0
Philip Fackler <facklerpw@ornl.gov>
2024-06-26 13:57:01 -0400

    Updated defiant-specific things

--- scripts/config_defiant.sh
+++ scripts/config_defiant.sh
@@ -3,11 +3,8 @@
 # Change the first 3 lines appropriately
 PROJ_DIR=/lustre/polis/csc266/scratch/4pf
 export JULIA_DEPOT_PATH=$PROJ_DIR/julia_depot
-MV_DIR=$PROJ_DIR/GrayScott.jl
-
-# remove existing generated Manifest.toml
-rm -f $MV_DIR/Manifest.toml
-rm -f $MV_DIR/LocalPreferences.toml
+MV_DIR=$PROJ_DIR/MiniVATES.jl
+echo $MV_DIR
 
 # good practice to avoid conflicts with existing default modules
 module purge
@@ -17,22 +14,22 @@ module load PrgEnv-cray-amd
 module load cray-mpich
 module load julia
 
+# remove existing generated Manifest.toml
+rm -f $MV_DIR/Manifest.toml
+rm -f $MV_DIR/LocalPreferences.toml
+
 # Required to point at underlying modules above
 export JULIA_AMDGPU_DISABLE_ARTIFACTS=1
 
-# MPIPreferences to use Cray's MPICH
-julia --project=$MV_DIR -e 'using MPIPreferences; MPIPreferences.use_system_binary(; library_names=["libmpi_cray"], mpiexec="srun")'
-
-# Regression being fixed with CUDA v4.0.0. CUDA.jl does lazy loading for portability to systems without NVIDIA GPUs
-# julia --project=$MV_DIR -e 'using Pkg; Pkg.add(name="CUDA", version="v3.13.1")' 
-# Adds a custom branch in case the development version is needed (for devs to test new features)
-julia --project=$MV_DIR -e 'using Pkg; Pkg.add("AMDGPU")'
-julia --project=$MV_DIR -e 'using JACC; JACC.JACCPreferences.set_backend("amdgpu")'
-julia --project -e 'using JACC.JACCPreferences; JACCPreferences.set_backend("CUDA")'
-
-# Instantiate the project by installing packages in Project.toml
 julia --project=$MV_DIR -e 'using Pkg; Pkg.instantiate()'
 
+# cray-mpich
+julia --project=$MV_DIR -e 'using Pkg; Pkg.add("MPIPreferences")'
+julia --project=$MV_DIR -e 'using MPIPreferences; MPIPreferences.use_system_binary(; library_names=["libmpi_cray"], mpiexec="srun")'
+
+# amdgpu
+julia --project=$MV_DIR -e 'using Pkg; Pkg.add("AMDGPU")'
+julia --project=$MV_DIR -e 'using JACC; JACC.JACCPreferences.set_backend("amdgpu")'
+
 # Verify the packages are installed correctly
-julia --project=$MV_DIR -e 'using Pkg; Pkg.build()'
-julia --project=$MV_DIR -e 'using Pkg; Pkg.precompile()'
+julia --project=$MV_DIR -e 'using Pkg; Pkg.instantiate()'
---
+++ scripts/job_defiant.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A CSC266
+#SBATCH -J MiniVATES.jl
+#SBATCH -o %x-%j.out
+#SBATCH -e %x-%j.err
+#SBATCH -t 0:10:00
+#SBATCH -p batch-gpu
+#SBATCH -N 1
+#SBATCH -G 4
+
+cd /lustre/polis/csc266/scratch/4pf/MiniVATES.jl
+
+srun -n 4 --gpus-per-task=1 julia --project test/benzil_corelli.jl
